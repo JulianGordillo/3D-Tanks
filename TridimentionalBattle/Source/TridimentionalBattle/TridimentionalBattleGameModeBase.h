23d461fa53e6c9e00922d67d5dcc781055e9b63e
maicolteam <49917994+maicolteam@users.noreply.github.com>
2019-05-23 04:38:11 +0900

    Project Creation

---
+++ TridimentionalBattle/Source/TridimentionalBattle/TridimentionalBattleGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TridimentionalBattleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRIDIMENTIONALBATTLE_API ATridimentionalBattleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
